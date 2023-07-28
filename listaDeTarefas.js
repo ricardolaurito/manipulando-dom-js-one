f78d75ba699ca6b4de1ad0ac7920be7a4a9c45cd
Ricardo Laurito <ricardolaurito@yahoo.com>
2023-07-28 01:53:47 -0500

    Concluir tarefa, ocultar escopo global

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,4 +1,4 @@
-const criarTarefa = (evento) => {
+( () => { const criarTarefa = (evento) => {
 
     evento.preventDefault()
 
@@ -8,12 +8,13 @@ const criarTarefa = (evento) => {
     
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    
+
     const conteudo = `<p class="content">${valor}</p>`
  
     tarefa.innerHTML = conteudo
 
     lista.appendChild(tarefa)
+    tarefa.appendChild(BotaoConclui())
     input.value = " "
 }
 
@@ -22,3 +23,23 @@ const novaTarefa = document.querySelector('[data-form-button]')
 
 
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
+}
+
+const concluirTarefa = (evento) => {
+
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement 
+
+    tarefaCompleta.classList.toggle('done')
+} 
+})()
